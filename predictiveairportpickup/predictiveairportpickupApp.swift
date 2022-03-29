af05e365477e9da31f734c34c20904b40a6bc1cc
benburns20 <benburns23@gmail.com>
2022-03-29 16:05:53 -0400

    Initial Commit

---
+++ predictiveairportpickup/predictiveairportpickupApp.swift
@@ -0,0 +1,17 @@
+//
+//  predictiveairportpickupApp.swift
+//  predictiveairportpickup
+//
+//  Created by Ben Burns on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct predictiveairportpickupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
